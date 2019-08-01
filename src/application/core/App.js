c4cf61d80f6f09dd11089541cd5a63f4b684ce1b
Tangomajom <kutyafika>
2019-08-01 15:44:00 +0200

    Basic side navigation components has been added to the application core.

--- src/application/core/App.js
+++ src/application/core/App.js
@@ -2,25 +2,51 @@ import React, {Component} from 'react';
 import {BrowserRouter, Route} from "react-router-dom";
 import Dashboard from "../dashboard/component/dashboard/Dashboard";
 import Header from "./components/header/Header";
+import {CssBaseline} from "@material-ui/core";
+import Navigation from "./components/navigation/Navigation";
+import {withStyles} from "@material-ui/styles";
 
 
-
+const styles = theme => ({
+    root: {
+        display: 'flex',
+    },
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing(3),
+    },
+    toolbar: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        padding: '0 8px',
+        ...theme.mixins.toolbar,
+    },
+});
 
 class App extends Component {
 
     constructor(props, context) {
         super(props, context);
-
     }
 
     render() {
+        const {classes} = this.props;
+
         return (
-            <BrowserRouter>
-                <Header/>
-                <Route path="/" component={Dashboard}/>
-            </BrowserRouter>
+            <div className={classes.root}>
+                <CssBaseline/>
+                <BrowserRouter>
+                    <Header/>
+                    <Navigation/>
+                    <main className={classes.content}>
+                        <div className={classes.toolbar} />
+                        <Route path="/" component={Dashboard}/>
+                    </main>
+                </BrowserRouter>
+            </div>
         );
     }
 }
 
-export default App;
+export default withStyles(styles, {withTheme: true})(App);
--- src/application/core/components/header/Header.js
+++ src/application/core/components/header/Header.js
@@ -1,20 +1,59 @@
 import React, {Component} from "react";
-import {AppBar,Toolbar,IconButton} from "@material-ui/core";
+import {AppBar, Toolbar, IconButton} from "@material-ui/core";
 import MenuIcon from '@material-ui/icons/Menu';
+import {withStyles} from "@material-ui/styles";
+import {drawerWidth} from "../navigation/Navigation";
+import clsx from "clsx";
 
-class Header extends Component{
+
+const styles = (theme) => ({
+    appBar: {
+        zIndex: theme.zIndex.drawer + 1,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+
+    },
+    menuButton: {
+        marginRight: 36,
+    },
+    appBarShift: {
+        marginLeft: drawerWidth,
+        width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+});
+
+
+class Header extends Component {
 
     constructor(props, context) {
         super(props, context);
+        this.state = {
+            open : true
+        }
     }
 
 
     render() {
+        const {classes} = this.props;
+
+        let open = this.state.open;
+
         return (
-            <AppBar position="static">
+            <AppBar
+                position="fixed"
+                className={clsx(classes.appBar, {
+                    [classes.appBarShift]: open,
+                })}
+            >
                 <Toolbar>
-                    <IconButton edge="start" color="inherit">
-                        <MenuIcon />
+                    <IconButton edge="start" color="inherit" className={classes.menuButton}>
+                        <MenuIcon/>
                     </IconButton>
                 </Toolbar>
             </AppBar>
@@ -22,4 +61,4 @@ class Header extends Component{
     }
 }
 
-export default Header;
+export default withStyles(styles,{withTheme : true})(Header);
