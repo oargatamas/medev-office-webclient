3df14d5f0d35ad5d7f4d308f90be5f141ae20abc
Tangomajom <kutyafika>
2019-08-02 14:53:34 +0200

    redux thunk inserted into store.

--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './application/core/App';
 import registerServiceWorker from './registerServiceWorker';
-import {createStore} from "redux";
+import {applyMiddleware, compose, createStore} from "redux";
 import {appReducer} from "./application/core/reducer/appReducer";
 import {Provider} from "react-redux";
 import {ThemeProvider} from "@material-ui/styles";
 import {theme} from "./application/core/theme/appTheme";
+import thunk from "redux-thunk";
 
-const store = createStore(appReducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(
+    appReducer,
+    compose(
+        applyMiddleware(thunk),
+        window.devToolsExtension ? window.devToolsExtension() : f => f
+    )
+);
 
 ReactDOM.render(
     <Provider store={store}>
