9e7eda25df8994063b7e1b3a2aa9252524420bfe
Tangomajom <kutyafika>
2019-08-20 11:51:57 +0200

    file upload api call base implementation has been added. (it can upload only one file, and not giving any feedback from the progress yet)

---
+++ src/application/drive/actions/uploadFileActions.js
@@ -0,0 +1,24 @@
+import {DRIVE_API_BASE} from "./driveApi";
+import {callOfficeApi} from "../../core/action/apiCallActions";
+import {defaultDialogErrorActions, defaultDialogFetchActions, defaultDialogSuccessActions} from "./dialogActions";
+
+
+export const deleteDriveItem = (folder, fileSource) => {
+
+    const uploadBody = new FormData();
+    uploadBody.append("fileItem",fileSource.files[0]); //Todo consider to handle multiple files
+
+    let params = {
+        method: "POST",
+        uri: DRIVE_API_BASE + "/" + folder.id + "/file",
+        redirect_uri: DRIVE_API_BASE+"/"+folder.id,
+        headers:{
+            "Content-Type" : "multipart/form-data"
+        },
+        body: uploadBody,
+        errorMsg: "Cannot upload file."
+    }; //Todo check how it can give back any progress.
+
+
+    return callOfficeApi(params, defaultDialogSuccessActions, defaultDialogErrorActions, defaultDialogFetchActions);
+};
